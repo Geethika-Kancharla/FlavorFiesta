741a3d8bf17f4a0028809d6e009cfee6ec8b93e2
Geethika-Kancharla <geethikak004@gmail.com>
2024-01-17 19:58:21 +0530

    About page added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { MenuBtn } from '../components/MenuBtn';
+import './Home.css';
+import AboutImg from '../utils/img/about-img.jpg';
+import { Link } from 'react-router-dom';
+
+function Home()
+{
+    return(
+        <div className='home-page'>
+            <header className='h-100 min-vh-100 d-flex align-items-center text-light shadow'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-sm-6 d-flex d-sm-block flex-column align-items-center'>
+                            <h2 className='mb-0 text-black fw-bold'>Welcome To</h2>
+                            <h1 className='mb-5 text-black fw-bold text-center text-sm-start'>React Restaurant</h1>
+                            <MenuBtn />
+                        </div>
+                    </div>
+                </div>
+            </header>
+            
+            <div className='container my-5'>
+                <div className='row'>
+                    <div className='col-lg-6 d-flex justify-content-center d-none d-lg-flex'>
+                        <img src={AboutImg} className='img-fluid w-50' alt="about img" />
+                    </div>
+                    <div className='col-lg-6 d-flex flex-column align-items-center justify-content-center'>
+                        <h2 className='fs-1 mb-5 text-uppercase fw-bold'>About Us</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident voluptate aut dolore ullam quasi numquam quod molestias cum officiis perspiciatis?</p>
+                        <p className='mb-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab magni animi tenetur eaque vel accusamus placeat quaerat ad. Similique quaerat qui doloribus assumenda deserunt tenetur quas suscipit officiis quod sequi?</p>
+                        <Link to="/about">
+                            <button type='button' className='btn btn-outline-success btn-lg'>More About Us</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+
+
+            </div>
+    )
+}
+export default Home;
